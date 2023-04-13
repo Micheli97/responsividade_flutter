9bd44353929eb08eb7adaf2db368379e8a375271
Paula Micheli <paula.michele.581@gmail.com>
2023-04-13 17:46:13 -0300

    feat(): Criando a seção do topo para mobile

--- responsividade/lib/pages/home/widgets/sections/top_section.dart
+++ responsividade/lib/pages/home/widgets/sections/top_section.dart
@@ -76,8 +76,7 @@ class TopSection extends StatelessWidget {
                     fit: BoxFit.cover,
                   ),
                 ),
-
-                 Positioned(
+                Positioned(
                     left: 20,
                     top: 20,
                     child: Card(
@@ -115,7 +114,44 @@ class TopSection extends StatelessWidget {
           );
         }
 
-        return Container();
+        return Column(
+          children: [
+            AspectRatio(
+              aspectRatio: 3.4,
+              child: Image.network(
+                "https://pbs.twimg.com/media/FSgl1OfUcAEsiQV.jpg:large",
+                // "https://pbs.twimg.com/media/FKNlhKZUcAEd7FY?format=jpg&name=4096x4096",
+                fit: BoxFit.cover,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text(
+                    "Aprenda Flutter com este curso",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "Bora aprender Flutter galerinha",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  CustomSearchField()
+                ],
+              ),
+            )
+          ],
+        );
       },
     );
   }
